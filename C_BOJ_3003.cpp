b5acc67620a447fa70595d3cd1e7814f1558a80e
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-11 09:17:59 +0900

    브론즈5

---
+++ C_BOJ_3003.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a,b,c,d,e,f=0;
+	int a1,b1,c1,d1,e1,f1=0;
+	a1=1;
+	b1=1;
+	c1=2;
+	d1=2;
+	e1=2;
+	f1=8;
+	scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+	printf("%d %d %d %d %d %d",a1-a,b1-b,c1-c,d1-d,e1-e,f1-f);
+}
